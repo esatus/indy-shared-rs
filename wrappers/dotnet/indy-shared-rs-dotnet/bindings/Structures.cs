cd74a7fa4c58d06ee6d51e713cc87997688fe8b8
pstenger <p.stenger@esatus.com>
2022-05-23 14:08:33 +0200

    .

--- wrappers/dotnet/indy-shared-rs-dotnet/bindings/Structures.cs
+++ wrappers/dotnet/indy-shared-rs-dotnet/bindings/Structures.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 namespace indy_shared_rs_dotnet.bindings
 {
 	public class Structures
 	{
+		[StructLayout(LayoutKind.Sequential)]
 		public struct FfiStrList
         {
 			public uint count;
