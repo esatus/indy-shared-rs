2f6f77648c0587f8e5d64b793ab90e46ab291cf9
SMichel-esatus <s.michel@esatus.com>
2022-05-25 15:54:38 +0200

    fix merge conflicts

--- wrappers/dotnet/indy-shared-rs-dotnet/indy-credx/CredentialApi.cs
+++ wrappers/dotnet/indy-shared-rs-dotnet/indy-credx/CredentialApi.cs
@@ -2,11 +2,7 @@
 
 namespace indy_shared_rs_dotnet.indy_credx
 {
-<<<<<<<< HEAD:wrappers/dotnet/indy-shared-rs-dotnet/indy-credx/PresentationRequestApi.cs
-    public static class PresentationRequestApi
-========
     public static class CredentialApi
->>>>>>>> 829b8ee5125266856ce7e2e31affa6e557a79df3:wrappers/dotnet/indy-shared-rs-dotnet/indy-credx/CredentialApi.cs
     {
         public static Task<string> GenerateNonceAsync()
         {
