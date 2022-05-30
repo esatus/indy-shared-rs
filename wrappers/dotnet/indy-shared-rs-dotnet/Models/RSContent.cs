87a8fc53e174c23b19a4591d1e5541e990971bb6
SMichel-esatus <s.michel@esatus.com>
2022-05-30 08:49:42 +0200

    Added RS

---
+++ wrappers/dotnet/indy-shared-rs-dotnet/Models/RSContent.cs
@@ -0,0 +1,6 @@
+﻿namespace indy_shared_rs_dotnet.Models
+{
+    public class RSContent
+    {
+    }
+}
---
+++ wrappers/dotnet/indy-shared-rs-dotnet/Models/RichSchema.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+
+namespace indy_shared_rs_dotnet.Models
+{
+    public class RichSchema
+    {
+        public string Id { get; set; }
+        public RSContent RsContent { get; set; }
+        [JsonProperty("rs_name")]
+        public string RsName { get; set; }
+        [JsonProperty("rs_version")]
+        public string RsVersion { get; set; }
+        [JsonProperty("rs_type")]
+        public string RsType { get; set; }
+        public string Ver { get; set; }
+    }
+}
