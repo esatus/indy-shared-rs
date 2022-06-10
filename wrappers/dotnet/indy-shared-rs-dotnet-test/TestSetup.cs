dcb7b4a8aab67318737f2f80e964e40ff6539120
Jan Wameling <j.wameling@esatus.com>
2022-06-10 09:27:40 +0200

    added preparations for presentationApiTests and its test setup

--- wrappers/dotnet/indy-shared-rs-dotnet-test/TestSetup.cs
+++ wrappers/dotnet/indy-shared-rs-dotnet-test/TestSetup.cs
@@ -2,8 +2,6 @@
 using indy_shared_rs_dotnet.Models;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace indy_shared_rs_dotnet_test
@@ -47,5 +45,99 @@ namespace indy_shared_rs_dotnet_test
             (CredentialDefinition credDef, CredentialDefinitionPrivate credDefPvt, CredentialKeyCorrectnessProof keyProof) =
                 await CredentialDefinitionApi.CreateCredentialDefinitionAsync(issuerDid, testSchema, "tag", "CL", 1);
         }**/
+
+        public static async Task<(PresentationRequest, Credential, RevocationRegistry, Schema, CredentialDefinition)> PrepareObjectsForPresentationTests()
+        {
+            // PresentationRequest
+            string nonce = await PresentationRequestApi.GenerateNonceAsync();
+            long timestamp = DateTimeOffset.Now.ToUnixTimeSeconds();
+            string presReqJson =
+                "{\"name\": \"proof\"," +
+                "\"version\": \"1.0\", " +
+                $"\"nonce\": \"{nonce}\"," +
+                "\"requested_attributes\": " +
+                "{" +
+                "\"reft\": " +
+                "{" +
+                "\"name\":\"attr\"," +
+                "\"non_revoked\":" +
+                "{ " +
+                $"\"from\": {timestamp}, " +
+                $"\"to\": {timestamp}" +
+                "}" +
+                "}" +
+                "}," +
+                "\"requested_predicates\": " +
+                "{" +
+                "\"light\": " +
+                "{" +
+                "\"name\":\"pred\"," +
+                "\"p_type\":\">=\"," +
+                "\"p_value\":18," +
+                "\"non_revoked\":" +
+                "{ " +
+                $"\"from\": {timestamp}, " +
+                $"\"to\": {timestamp}" +
+                "}" +
+                "}" +
+                "}," +
+                "\"non_revoked\": " +
+                "{ " +
+                $"\"from\": {timestamp}," +
+                $"\"to\": {timestamp}" +
+                "}," +
+                "\"ver\": \"1.0\"" +
+                "}";
+            PresentationRequest presentationRequest = await PresentationRequestApi.CreatePresReqFromJsonAsync(presReqJson);
+
+            // CredentialDefinition
+            List<string> attrNames = new() { "gender", "age", "sex" };
+            string issuerDid = "NcYxiDXkpYi6ov5FcYDi1e";
+            string schemaName = "gvt";
+            string schemaVersion = "1.0";
+
+            Schema schemaObject = await SchemaApi.CreateSchemaAsync(issuerDid, schemaName, schemaVersion, attrNames, 0);
+
+            (CredentialDefinition credDef, CredentialDefinitionPrivate credDefPvt, CredentialKeyCorrectnessProof keyProof) =
+                await CredentialDefinitionApi.CreateCredentialDefinitionAsync(issuerDid, schemaObject, "tag", "CL", 1);
+
+            // Schema
+            List<string> schemattrNames = new() { "gender", "age", "sex" };
+            string schemaIssuerDid = "NcYxiDXkpYi6ov5FcYDi1e";
+            string schemaSchemaName = "gvt";
+            string schemaSchemaVersion = "1.0A";
+
+            Schema schema = await SchemaApi.CreateSchemaAsync(schemaIssuerDid, schemaSchemaName, schemaSchemaVersion, schemattrNames, 0);
+
+            // CredentialObject, RevObject
+            List<string> credentialObjectAttrNames = new() { "name", "age", "sex" };
+            List<string> attrNamesRaw = new() { "Alex", "20", "male" };
+            List<string> attrNamesEnc = new() { "Alex", "20", "male" };
+            string credentialObjectIssuerDid = "NcYxiDXkpYi6ov5FcYDi1e";
+            string credentialObjectProverDid = "VsKV7grR1BUE29mG2Fm2kX";
+            string credentialObjectSchemaName = "gvt";
+            string credentialObjectSchemaVersion = "1.0";
+
+            MasterSecret masterSecretObject = await MasterSecretApi.CreateMasterSecretAsync();
+
+            Schema credentialObjectSchemaNameSchemaObject = await SchemaApi.CreateSchemaAsync(credentialObjectIssuerDid, credentialObjectSchemaName, credentialObjectSchemaVersion, credentialObjectAttrNames, 0);
+            (CredentialDefinition credDefObject, CredentialDefinitionPrivate credDefPvtObject, CredentialKeyCorrectnessProof keyProofObject) =
+                await CredentialDefinitionApi.CreateCredentialDefinitionAsync(issuerDid, schemaObject, "tag", "CL", 1);
+
+            string schemaId = await CredentialDefinitionApi.GetCredentialDefinitionAttribute(credDefObject, "schema_id");
+            CredentialOffer credOfferObject = await CredentialOfferApi.CreateCredentialOfferAsync(schemaId, credDefObject, keyProofObject);
+
+            (CredentialRequest credRequestObject, CredentialRequestMetadata metaDataObject) =
+                await CredentialRequestApi.CreateCredentialRequestAsync(credentialObjectProverDid, credDefObject, masterSecretObject, "testMasterSecretName", credOfferObject);
+
+            //Todo fix class CredentialRevocationInfo
+            CredentialRevocationInfo credRevInfo = null;
+
+            (Credential credObject, RevocationRegistry revRegObject, RevocationDelta revDeltaObject) =
+                await CredentialApi.CreateCredentialAsync(credDefObject, credDefPvtObject, credOfferObject, credRequestObject,
+                attrNames, attrNamesRaw, attrNamesEnc, credRevInfo);
+
+            return (presentationRequest, credObject, revRegObject, schema, credDef);
+        }
     }
 }
---
+++ wrappers/dotnet/indy-shared-rs-dotnet-test/indy-credx/PresentationApiTests.cs
@@ -0,0 +1,86 @@
+﻿using FluentAssertions;
+using indy_shared_rs_dotnet.indy_credx;
+using indy_shared_rs_dotnet.Models;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace indy_shared_rs_dotnet_test.indy_credx
+{
+    public class PresentationApiTests
+    {
+        [Test]
+        [TestCase(TestName = "CreatePresentationAsync works.")]
+        public async Task CreatePresentation()
+        {
+            //Arrange
+            (PresentationRequest presentationRequest,
+                Credential credential,
+                RevocationRegistry revocation,
+                Schema schema,
+                CredentialDefinition credentialDefinition) = await TestSetup.PrepareObjectsForPresentationTests();
+
+
+            List<CredentialEntry> credentialEntries = new()
+            {
+                new CredentialEntry
+                {
+                    CredentialObjectHandle = credential.Handle,
+                    Timestamp = DateTimeOffset.Now.ToUnixTimeSeconds(),
+                    RevStateObjectHandle = revocation.Handle
+                }
+            };
+
+            List<CredentialProve> credentialProves = new()
+            {
+                new CredentialProve
+                {
+                    EntryIndex = 1,
+                    IsPredicate = Convert.ToByte(false),
+                    Referent = "testReferent",
+                    Reveal = Convert.ToByte(false)
+                }
+            };
+
+            List<string> selfAttestNames = new()
+            {
+                "testSelfAttestName1"
+            };
+
+            List<string> selfAttestValues = new()
+            {
+                "testSelfAttestName1"
+            };
+
+            MasterSecret masterSecret = await MasterSecretApi.CreateMasterSecretAsync();
+
+            List<Schema> schemas = new()
+            {
+               schema
+            };
+
+            List<CredentialDefinition> credentialDefinitions = new()
+            {
+                credentialDefinition
+            };
+
+            //Act
+            Presentation actual = await PresentationApi.CreatePresentationAsync(
+                presentationRequest,
+                credentialEntries,
+                credentialProves,
+                selfAttestNames,
+                selfAttestValues,
+                masterSecret,
+                schemas,
+                credentialDefinitions
+                );
+
+            //Assert
+
+            actual.Should().BeOfType(typeof(Presentation));
+        }
+
+    }
+}
