27e3713782b8dc1428495966844297bb3467ac19
HPfaff-esatus <h.pfaff@esatus.com>
2022-06-15 09:53:32 +0200

    Added tests for CredentialRequestApi

--- wrappers/dotnet/indy-shared-rs-dotnet-test/indy-credx/CredentialRequestApiTests.cs
+++ wrappers/dotnet/indy-shared-rs-dotnet-test/indy-credx/CredentialRequestApiTests.cs
@@ -41,18 +41,76 @@ namespace indy_shared_rs_dotnet_test.indy_credx
 
         private static IEnumerable<TestCaseData> CreateCredentialRequestCases()
         {
-            yield return new TestCaseData()
-                .SetName("");
+            yield return new TestCaseData(false, false, false, false, false)
+                .SetName("CreateCredentialRequestAsync() throws Exception if all arguments are null.");
+            yield return new TestCaseData(false, true, true, true, true)
+                .SetName("CreateCredentialRequestAsync() throws Exception if proverDid is null.");
+            yield return new TestCaseData(true, false, true, true, true)
+                .SetName("CreateCredentialRequestAsync() throws Exception if credential defintion is null.");
+            yield return new TestCaseData(true, true, false, true, true)
+                .SetName("CreateCredentialRequestAsync() throws Exception if master secret is null.");
+            yield return new TestCaseData(true, true, true, false, true)
+                .SetName("CreateCredentialRequestAsync() throws Exception if master secret id is null.");
+            yield return new TestCaseData(true, true, true, true, false)
+                .SetName("CreateCredentialRequestAsync() throws Exception if credential offer is null.");
         }
 
         [Test, TestCaseSource(nameof(CreateCredentialRequestCases))]
-        public async Task CreateCredentialRequestThrowsException()
+        public async Task CreateCredentialRequestThrowsException(bool hasProverDid, bool hasCredDef, bool hasMasterSecret, bool hasMasterSecretId, bool hasCredentialOffer)
         {
             //Arrange
+            string proverDid = null;
+            CredentialDefinition credDefObject = null;
+            MasterSecret masterSecretObject = null;
+            string masterSecretId = null;
+            CredentialOffer credOfferObject = null;
+
+            if (hasProverDid)
+            {
+                proverDid = "VsKV7grR1BUE29mG2Fm2kX";
+            }
+
+            if (hasCredDef)
+            {
+                List<string> attrNames = new() { "gender", "age", "sex" };
+                string issuerDid = "NcYxiDXkpYi6ov5FcYDi1e";
+                proverDid = "VsKV7grR1BUE29mG2Fm2kX";
+                string schemaName = "gvt";
+                string schemaVersion = "1.0";
+                Schema schemaObject = await SchemaApi.CreateSchemaAsync(issuerDid, schemaName, schemaVersion, attrNames, 0);
+                (CredentialDefinition tmpCredDef, _, _) =
+                await CredentialDefinitionApi.CreateCredentialDefinitionAsync(issuerDid, schemaObject, "tag", Consts.SIGNATURE_TYPE, 1);
+                credDefObject = tmpCredDef;
+            }
+
+            if (hasMasterSecret)
+            {
+                masterSecretObject = await MasterSecretApi.CreateMasterSecretAsync();
+            }
+
+            if (hasMasterSecretId)
+            {
+                masterSecretId = "testMasterSecretName";
+            }
+
+            if (hasCredentialOffer)
+            {
+                string issuerDid = "NcYxiDXkpYi6ov5FcYDi1e";
+                List<string> attrNames = new() { "gender", "age", "sex" };
+                string schemaName = "gvt";
+                string schemaVersion = "1.0";
+                Schema schemaObject = await SchemaApi.CreateSchemaAsync(issuerDid, schemaName, schemaVersion, attrNames, 0);
+                (CredentialDefinition tmpCredDef, _, CredentialKeyCorrectnessProof keyProofObject) =
+                   await CredentialDefinitionApi.CreateCredentialDefinitionAsync(issuerDid, schemaObject, "tag", Consts.SIGNATURE_TYPE, 1);
+                string schemaId = await CredentialDefinitionApi.GetCredentialDefinitionAttributeAsync(tmpCredDef, "schema_id");
+                credOfferObject = await CredentialOfferApi.CreateCredentialOfferAsync(schemaId, tmpCredDef, keyProofObject);
+            }
 
             //Act
+            Func<Task> act = async () => await CredentialRequestApi.CreateCredentialRequestAsync(proverDid, credDefObject, masterSecretObject, masterSecretId, credOfferObject);
 
             //Assert
+            await act.Should().ThrowAsync<Exception>();
         }
     }
 }
