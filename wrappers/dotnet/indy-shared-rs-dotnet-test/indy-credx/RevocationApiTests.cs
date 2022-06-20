12d0ae1236974235e25508feeeb2bc1c8ff22765
HPfaff-esatus <h.pfaff@esatus.com>
2022-06-20 17:07:54 +0200

    Added tests for revocation API

--- wrappers/dotnet/indy-shared-rs-dotnet-test/indy-credx/RevocationApiTests.cs
+++ wrappers/dotnet/indy-shared-rs-dotnet-test/indy-credx/RevocationApiTests.cs
@@ -50,10 +50,10 @@ namespace indy_shared_rs_dotnet_test.indy_credx
 
             Schema schemaObject = await SchemaApi.CreateSchemaAsync(issuerDid, schemaName, schemaVersion, attrNames, 0);
 
-            (CredentialDefinition credDef, CredentialDefinitionPrivate credDefPvt, CredentialKeyCorrectnessProof keyProof) =
+            (CredentialDefinition credDef, _, _) =
                 await CredentialDefinitionApi.CreateCredentialDefinitionAsync(issuerDid, schemaObject, "tag", Consts.SIGNATURE_TYPE, 1);
 
-            (RevocationRegistryDefinition revRegDefObject, RevocationRegistryDefinitionPrivate revRegDefPvtObject, RevocationRegistry tmpRevRegObject, RevocationRegistryDelta tmpRevRegDeltaObject) =
+            (RevocationRegistryDefinition revRegDefObject, _, RevocationRegistry tmpRevRegObject, _) =
                 await RevocationApi.CreateRevocationRegistryAsync(issuerDid, credDef, "test_tag", RegistryType.CL_ACCUM, IssuerType.ISSUANCE_BY_DEFAULT, 99, testTailsPath);
 
             List<long> issuedList = new List<long> { 0 };
@@ -66,11 +66,11 @@ namespace indy_shared_rs_dotnet_test.indy_credx
                     tmpRevRegObject,
                     issuedList,
                     revokedList,
-                    testTailsPath
+                    revRegDefObject.Value.TailsLocation
                     );
 
             //Assert
-            revRegObject.Should().NotBeNull();
+            revRegObject.Value.Should().NotBeSameAs(tmpRevRegObject.Value);
             revRegDeltaObject.Should().NotBeNull();
         }
         #endregion
@@ -80,10 +80,34 @@ namespace indy_shared_rs_dotnet_test.indy_credx
         public async Task RevokeCredentialWorks()
         {
             //Arrange
+            List<string> attrNames = new() { "gender", "age", "sex" };
+            string issuerDid = "NcYxiDXkpYi6ov5FcYDi1e";
+            string schemaName = "gvt";
+            string schemaVersion = "1.0";
+            string testTailsPath = null;
+
+            Schema schemaObject = await SchemaApi.CreateSchemaAsync(issuerDid, schemaName, schemaVersion, attrNames, 0);
+            (CredentialDefinition credDef, 
+                _, 
+                _) = await CredentialDefinitionApi.CreateCredentialDefinitionAsync(issuerDid, schemaObject, "tag", Consts.SIGNATURE_TYPE, 1);
+            
+            (RevocationRegistryDefinition revRegDefObject, 
+                _, 
+                RevocationRegistry tmpRevRegObject, 
+                _) = await RevocationApi.CreateRevocationRegistryAsync(issuerDid, credDef, "test_tag", RegistryType.CL_ACCUM, IssuerType.ISSUANCE_BY_DEFAULT, 99, testTailsPath);
 
             //Act
+            (_, RevocationRegistryDelta actual) = 
+                await RevocationApi.RevokeCredentialAsync(
+                    revRegDefObject,
+                    tmpRevRegObject,
+                    0,
+                    revRegDefObject.Value.TailsLocation
+                    );
 
             //Assert
+            actual.Value.PrevAccum.Should().NotBeNull();
+            actual.Value.Revoked.Should().HaveCount(1);
         }
         #endregion
 
@@ -92,10 +116,46 @@ namespace indy_shared_rs_dotnet_test.indy_credx
         public async Task MergeRevocationRegistryDeltasWorks()
         {
             //Arrange
+            List<string> attrNames = new() { "gender", "age", "sex" };
+            string issuerDid = "NcYxiDXkpYi6ov5FcYDi1e";
+            string schemaName = "gvt";
+            string schemaVersion = "1.0";
+            string testTailsPath = null;
+
+            Schema schemaObject = await SchemaApi.CreateSchemaAsync(issuerDid, schemaName, schemaVersion, attrNames, 0);
+
+            (CredentialDefinition credDef, _, _) =
+                await CredentialDefinitionApi.CreateCredentialDefinitionAsync(issuerDid, schemaObject, "tag", Consts.SIGNATURE_TYPE, 1);
+
+            (RevocationRegistryDefinition revRegDefObject, _, RevocationRegistry revRegObject, _) =
+                await RevocationApi.CreateRevocationRegistryAsync(issuerDid, credDef, "test_tag", RegistryType.CL_ACCUM, IssuerType.ISSUANCE_BY_DEFAULT, 99, testTailsPath);
+
+            List<long> issuedList1 = new List<long> { 0,2 };
+            List<long> issuedList2 = new List<long> { 0,3 };
+            List<long> revokedList1 = new List<long> { 0,2 };
+            List<long> revokedList2 = new List<long> { 0,3 };
+
+            (RevocationRegistry tmpRevReg, RevocationRegistryDelta delta1) = await RevocationApi.UpdateRevocationRegistryAsync(
+                revRegDefObject,
+                revRegObject,
+                issuedList1,
+                revokedList1,
+                revRegDefObject.Value.TailsLocation
+                );
+            (RevocationRegistry tmpRevReg2, RevocationRegistryDelta delta2) = await RevocationApi.UpdateRevocationRegistryAsync(
+                revRegDefObject,
+                revRegObject,
+                issuedList2,
+                revokedList2,
+                revRegDefObject.Value.TailsLocation
+                );
 
             //Act
+            RevocationRegistryDelta actual = await RevocationApi.MergeRevocationRegistryDeltasAsync(delta1, delta2);
 
             //Assert
+            actual.Value.Revoked.Should().HaveCount(1);
+            actual.Value.Revoked.Contains(2).Should().BeTrue();
         }
         #endregion
 
@@ -104,10 +164,36 @@ namespace indy_shared_rs_dotnet_test.indy_credx
         public async Task CreateOrUpdateRevocationStateWorks()
         {
             //Arrange
+            List<string> attrNames = new() { "gender", "age", "sex" };
+            string issuerDid = "NcYxiDXkpYi6ov5FcYDi1e";
+            string schemaName = "gvt";
+            string schemaVersion = "1.0";
+            string testTailsPath = null;
+
+            Schema schemaObject = await SchemaApi.CreateSchemaAsync(issuerDid, schemaName, schemaVersion, attrNames, 0);
+
+            (CredentialDefinition credDef,
+                _,
+                _) = await CredentialDefinitionApi.CreateCredentialDefinitionAsync(issuerDid, schemaObject, "tag", Consts.SIGNATURE_TYPE, 1);
+
+
+            (RevocationRegistryDefinition revRegDefObject, _, RevocationRegistry tmpRevRegObject, RevocationRegistryDelta revRegDeltaObject) =
+                await RevocationApi.CreateRevocationRegistryAsync(issuerDid, credDef, "test_tag", RegistryType.CL_ACCUM, IssuerType.ISSUANCE_BY_DEFAULT, 99, testTailsPath);
+
+            CredentialRevocationState revState = new();
 
             //Act
+            CredentialRevocationState actual = await RevocationApi.CreateOrUpdateRevocationStateAsync(
+                revRegDefObject,
+                revRegDeltaObject,
+                0,
+                0,
+                revRegDefObject.Value.TailsLocation,
+                revState
+                );
 
             //Assert
+            actual.Should().NotBeNull();
         }
         #endregion
 
@@ -116,10 +202,31 @@ namespace indy_shared_rs_dotnet_test.indy_credx
         public async Task GetRevocationRegistryDefinitionAttributeWorks()
         {
             //Arrange
+            List<string> attrNames = new() { "gender", "age", "sex" };
+            string issuerDid = "NcYxiDXkpYi6ov5FcYDi1e";
+            string schemaName = "gvt";
+            string schemaVersion = "1.0";
+            string testTailsPath = null;
+
+            Schema schemaObject = await SchemaApi.CreateSchemaAsync(issuerDid, schemaName, schemaVersion, attrNames, 0);
+
+            (CredentialDefinition credDef,
+                _,
+                _) = await CredentialDefinitionApi.CreateCredentialDefinitionAsync(issuerDid, schemaObject, "tag", Consts.SIGNATURE_TYPE, 1);
+
+            (RevocationRegistryDefinition revRegDefObject, 
+                _, 
+                _, 
+                _) = await RevocationApi.CreateRevocationRegistryAsync(issuerDid, credDef, "test_tag", RegistryType.CL_ACCUM, IssuerType.ISSUANCE_BY_DEFAULT, 99, testTailsPath);
 
             //Act
+            string actual = await RevocationApi.GetRevocationRegistryDefinitionAttributeAsync(
+                revRegDefObject,
+                "id"
+                );
 
             //Assert
+            actual.Should().NotBeNull();
         }
         #endregion
     }
