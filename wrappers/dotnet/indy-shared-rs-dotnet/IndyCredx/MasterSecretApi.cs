65fba3e83ee238af317c80272f91ae8edb32847b
Jan Wameling <j.wameling@esatus.com>
2022-07-27 10:57:50 +0200

    added json method in MasterSecretApi

--- wrappers/dotnet/indy-shared-rs-dotnet/IndyCredx/MasterSecretApi.cs
+++ wrappers/dotnet/indy-shared-rs-dotnet/IndyCredx/MasterSecretApi.cs
@@ -30,6 +30,27 @@ namespace indy_shared_rs_dotnet.IndyCredx
             msObject.Handle = result;
             return await Task.FromResult(msObject);
         }
+        /// <summary>
+        /// Creates a new <see cref="MasterSecret"/>.
+        /// </summary>
+        /// <exception cref="SharedRsException">Throws when <see cref="MasterSecret"/> can't be created.</exception>
+        /// <returns>New <see cref="MasterSecret"/>.</returns>
+        public static async Task<string> CreateMasterSecretJsonAsync()
+        {
+            IntPtr result = new IntPtr();
+            int errorCode = NativeMethods.credx_create_master_secret(ref result);
+
+            if (errorCode != 0)
+            {
+                string error = await ErrorApi.GetCurrentErrorAsync();
+                throw SharedRsException.FromSdkError(error);
+            }
+
+            string masterSecretJson = await ObjectApi.ToJsonAsync(result);
+
+            return await Task.FromResult(masterSecretJson);
+        }
+
         private static async Task<MasterSecret> CreateMasterSecretObject(IntPtr objectHandle)
         {
             string masterSecretJson = await ObjectApi.ToJsonAsync(objectHandle);
