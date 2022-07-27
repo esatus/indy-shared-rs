a931d7af6e1f055682c32c0beb3e033403ce8e8e
Jan Wameling <j.wameling@esatus.com>
2022-07-27 11:04:12 +0200

    added json method in CredentialRequestApi

--- wrappers/dotnet/indy-shared-rs-dotnet/IndyCredx/CredentialRequestApi.cs
+++ wrappers/dotnet/indy-shared-rs-dotnet/IndyCredx/CredentialRequestApi.cs
@@ -46,6 +46,54 @@ namespace indy_shared_rs_dotnet.IndyCredx
             return (requestObject, metadataObject);
         }
 
+        /// <summary>
+        /// Creates a new <see cref="CredentialRequest"/> from <see cref="CredentialDefinition"/>.
+        /// </summary>
+        /// <param name="proverDid">Prover DID.</param>
+        /// <param name="credentialDefinition">Credential definition.</param>
+        /// <param name="masterSecret">New master secret.</param>
+        /// <param name="masterSecretId">Id of master secret.</param>
+        /// <param name="credentialOffer">Credential offer.</param>
+        /// <exception cref="SharedRsException">Throws if any argument is invalid.</exception>
+        /// <returns>New <see cref="CredentialRequest"/> as JSON string and its <see cref="CredentialRequestMetadata"/> as JSON string.</returns>
+        public static async Task<(string, string)> CreateCredentialRequestAsync(
+            string proverDid,
+            string credentialDefinitionJson,
+            string masterSecretJson,
+            string masterSecretId,
+            string credentialOfferJson)
+        {
+            IntPtr credDefObjectHandle = new IntPtr();
+            IntPtr masterSecretObjectHandle = new IntPtr();
+            IntPtr credentialOfferObjectHandle = new IntPtr();
+
+            _ = NativeMethods.credx_credential_definition_from_json(ByteBuffer.Create(credentialDefinitionJson), ref credDefObjectHandle);
+            _ = NativeMethods.credx_master_secret_from_json(ByteBuffer.Create(masterSecretJson), ref masterSecretObjectHandle);
+            _ = NativeMethods.credx_credential_offer_from_json(ByteBuffer.Create(credentialOfferJson), ref credentialOfferObjectHandle);
+
+            IntPtr requestHandle = new IntPtr();
+            IntPtr metadataHandle = new IntPtr();
+            int errorCode = NativeMethods.credx_create_credential_request(
+                FfiStr.Create(proverDid),
+                credDefObjectHandle,
+                masterSecretObjectHandle,
+                FfiStr.Create(masterSecretId),
+                credentialOfferObjectHandle,
+                ref requestHandle,
+                ref metadataHandle);
+
+            if (errorCode != 0)
+            {
+                string error = await ErrorApi.GetCurrentErrorAsync();
+                throw SharedRsException.FromSdkError(error);
+            }
+            string requestJson = await ObjectApi.ToJsonAsync(requestHandle);
+            string metadataJson = await ObjectApi.ToJsonAsync(metadataHandle);
+
+            return (requestJson, metadataJson);
+        }
+
+        #region private methods
         private static async Task<CredentialRequest> CreateCredentialRequestObject(IntPtr objectHandle)
         {
             string credReqJson = await ObjectApi.ToJsonAsync(objectHandle);
@@ -90,5 +138,6 @@ namespace indy_shared_rs_dotnet.IndyCredx
             CredentialRequestMetadata result = await CreateCredentialRequestMetadataObject(credReqHandle);
             return await Task.FromResult(result);
         }
+        #endregion
     }
 }
